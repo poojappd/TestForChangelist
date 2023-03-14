ccb91c82864c0b0b767a6602ae95010d064aed80
pooja-14598 <poojapriyadarshnips@gmail.com>
2023-03-14 15:03:37 +0530

    idk

---
+++ app/src/main/java/com/example/testforchangelist/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.testforchangelist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//from comments changelist
+//still from comments
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+//from changes changelist
